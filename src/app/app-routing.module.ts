82bb57c28158cf48491fc450ef1f1b2d20c50f23
AbdulmalikAdeleke <68475114+AbdulmalikAdeleke@users.noreply.github.com>
2021-06-03 13:49:32 +0100

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminnavComponent } from './adminnav/adminnav.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'staff', 
+    loadChildren: () => import('./staff/staff.module')
+                        .then(m => m.StaffModule)
+  },
+  {
+    path: 'admin', 
+    component: AdminnavComponent,
+    loadChildren: () => import('./admin/admin.module')
+                        .then(m => m.AdminModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
